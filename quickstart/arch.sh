4104004cc8583bfb815ca6b291f21a70fc932a28
Warbacon <joaquinguerratocino@gmail.com>
2023-11-22 01:06:39 +0100

    Remove arch quickstart

--- quickstart/arch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Should be used after installing the "gnome" package.
-
-sudo pacman -Rns gnome-calendar epiphany gnome-music gnome-user-docs yelp gnome-software gnome-text-editor gnome-connections gnome-contacts gnome-weather gnome-clocks gnome-maps gnome-photos totem simple-scan gnome-system-monitor cheese gnome-tour baobab gnome-console
