685b49396dc585f633d787e6374bbab42966c998
Warbacon <45147327+warbacon@users.noreply.github.com>
2024-01-24 21:49:14 +0100

    Update wezterm config

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -14,7 +14,7 @@ end
 -- config.front_end = "WebGpu"
 
 -- Shell
--- config.default_prog = { "zsh" }
+config.default_prog = { "fish" }
 -- config.default_prog = { "pwsh", "-NoLogo" }
 
 -- Font
@@ -24,6 +24,7 @@ config.unicode_version = 14
 config.warn_about_missing_glyphs = false
 
 -- UI
+config.color_scheme = "Catppuccin Mocha"
 config.window_padding = {
 	left = 0,
 	right = 0,
@@ -46,53 +47,6 @@ config.keys = {
 		key = "w",
 		mods = "CTRL|SHIFT",
 		action = wezterm.action.CloseCurrentPane({ confirm = true }),
-	},
-}
-
--- Kanagawa
-config.force_reverse_video_cursor = true
-config.colors = {
-	foreground = "#dcd7ba",
-	background = "#1f1f28",
-
-	cursor_bg = "#c8c093",
-	cursor_fg = "#c8c093",
-	cursor_border = "#c8c093",
-
-	selection_fg = "#c8c093",
-	selection_bg = "#2d4f67",
-
-	scrollbar_thumb = "#16161d",
-	split = "#16161d",
-
-	ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
-	brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
-	indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
-
-	tab_bar = {
-		background = "#16161D",
-		active_tab = {
-			bg_color = "#1F1F28",
-			fg_color = "#DCD7BA",
-		},
-		inactive_tab = {
-			bg_color = "#16161D",
-			fg_color = "#C8C093",
-		},
-		inactive_tab_hover = {
-			bg_color = "#2A2A37",
-			fg_color = "#C8C093",
-			italic = false,
-		},
-		new_tab = {
-			bg_color = "#16161D",
-			fg_color = "#C8C093",
-		},
-		new_tab_hover = {
-			bg_color = "#2A2A37",
-			fg_color = "#C8C093",
-			italic = false,
-		},
 	},
 }
 
