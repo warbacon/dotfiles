71099e34d3bd34248773c9cbb60c45bca7a42059
Warbacon <45147327+warbacon@users.noreply.github.com>
2024-01-24 21:34:49 +0100

    Update fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,11 +1,17 @@
-set -gx EDITOR nvim
-set -gx VISUAL nvim
+# ENVIROMENT VARIABLES --------------------------------------------------------
+if command -q nvim
+    set -gx EDITOR nvim
+    set -gx VISUAL nvim
+    set -gx MANPAGER 'nvim +Man!'
+end
+# -----------------------------------------------------------------------------
 
-status is-interactive
+status is-interactive  # Do anything if isn't interactive
 or return
 
-set -g fish_greeting
+set -g fish_greeting  # Disable fish_greeting
 
+# ABBREVIATIONS ---------------------------------------------------------------
 command -q fastfetch
 and abbr -a ffetch fastfetch
 
@@ -26,6 +32,17 @@ if command -q eza
     abbr -a lt eza --icons -T
 end
 
+# KEYBINDINGS -----------------------------------------------------------------
+bind \eg "go_dir $HOME/Git"
+bind \ec "go_dir $HOME/.config"
+
+# MODIFY $PATH ----------------------------------------------------------------
+fish_add_path -P "$HOME/.local/bin"
+
+test -f "/opt/miniconda3/etc/fish/conf.d/conda.fish"
+and source "/opt/miniconda3/etc/fish/conf.d/conda.fish"
+
+# PROMPT ----------------------------------------------------------------------
 function prompt_hook --on-event fish_prompt
     test -z $add_newline; and set -g add_newline true; or printf "\n"
 
@@ -33,14 +50,6 @@ function prompt_hook --on-event fish_prompt
     and abbr -a ll eza --icons --group-directories-first -l --git --git-repos
     or abbr -a ll eza --icons --group-directories-first -l --git
 end
-
-bind \eg "go_dir $HOME/Git"
-bind \ec "go_dir $HOME/.config"
-
-fish_add_path -P "$HOME/.local/bin"
-
-test -f "/opt/miniconda3/etc/fish/conf.d/conda.fish"
-and source "/opt/miniconda3/etc/fish/conf.d/conda.fish"
 
 command -q starship
 and starship init fish --print-full-init | source
