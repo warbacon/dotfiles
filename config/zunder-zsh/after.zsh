696e67f363c9d3f3b88f4f76d4ec288ee6b0b11b
Warbacon <45147327+warbacon@users.noreply.github.com>
2024-01-30 21:26:54 +0100

    Update zunder-zsh config

--- config/zunder-zsh/after.zsh
+++ config/zunder-zsh/after.zsh
@@ -5,11 +5,45 @@ export VISUAL=nvim
 # ALIASES ----------------------------------------------------------------------
 [[ -n $commands[trash] ]] && alias rm="trash" || echo "trash-cli is not installed."
 [[ $TERM == "xterm-kitty" ]] && alias icat="kitten icat"
+[[ -n $commands[lazygit] ]] && alias lg="lazygit"
 
-# NEOVIM CURSOR FIX ------------------------------------------------------------
-if [[ -z $KITTY_SHELL_INTEGRATION && -n $commands[nvim] ]]; then
-  function nvim() { $commands[nvim] "$@"; printf "\e[0 q" }
-fi
+# GO_REPO ----------------------------------------------------------------------
+GIT_REPOS_DIR="$HOME/Git"
+
+function go_repo() {
+  dir="$(eza -D "$GIT_REPOS_DIR" | fzf)"
+
+  if [[ -z "$dir" ]]; then
+    zle redisplay
+    return 0
+  fi
+
+  zle push-line
+  BUFFER="cd $GIT_REPOS_DIR/$dir"
+  zle accept-line
+}
+
+zle -N go_repo
+bindkey -e "^[g" go_repo
+
+# GO_CONFIG --------------------------------------------------------------------
+CONFIG_DIR="$HOME/.config"
+
+function go_config() {
+  dir="$(eza -D "$CONFIG_DIR" | fzf)"
+
+  if [[ -z "$dir" ]]; then
+    zle redisplay
+    return 0
+  fi
+
+  zle push-line
+  BUFFER="cd $CONFIG_DIR/$dir"
+  zle accept-line
+}
+
+zle -N go_config
+bindkey -e "^[c" go_config
 
 # START TMUX -------------------------------------------------------------------
 # if [[ -z ${TMUX+X}${ZSH_SCRIPT+X}${ZSH_EXECUTION_STRING+X} ]]; then
--- config/zunder-zsh/before.zsh
+++ config/zunder-zsh/before.zsh
@@ -1,13 +1,20 @@
 # ZUNDER-ZSH OPTIONS -----------------------------------------------------------
-# DISABLE_AUTOSUGGESTIONS=true
-# DISABLE_SYNTAX_HIGHLIGHTING=true
+DISABLE_AUTOSUGGESTIONS=true
+DISABLE_SYNTAX_HIGHLIGHTING=true
 # DISABLE_EXA=true
 
 # ZUNDER-PROMPT ----------------------------------------------------------------
 # DISABLE_ZUNDER_PROMPT=true
+# PS1="[$USER@$HOST %1~]%# "
 # zinit snippet "$HOME/Git/zunder-prompt/zunder-prompt.plugin.zsh"
 # zinit snippet "$ZUNDER_ZSH_DIR/prompt.zsh"
 ZUNDER_PROMPT_CHAR="%B>%b"
+
+# STARSHIP ---------------------------------------------------------------------
+# zinit as"command" from"gh-r" \
+#   atclone"./starship init zsh > init.zsh; ./starship completions zsh > _starship" \
+#   atpull"%atclone" src"init.zsh" for \
+#   starship/starship
 
 # OH-MY-POSH -------------------------------------------------------------------
 # zinit as"program" from"gh-r" mv"posh* -> oh-my-posh" \
@@ -16,17 +23,11 @@ ZUNDER_PROMPT_CHAR="%B>%b"
 #     atpull"%atclone" src"init.zsh" for \
 #     "jandedobbeleer/oh-my-posh"
 
-# STARSHIP ---------------------------------------------------------------------
-# zinit as"command" from"gh-r" \
-#   atclone"./starship init zsh > init.zsh; ./starship completions zsh > _starship" \
-#   atpull"%atclone" src"init.zsh" for \
-#   starship/starship
-
 # FZF INTEGRATION --------------------------------------------------------------
-FZF_CTRL_T_COMMAND="fd -H --exclude=node_modules --exclude=Trash"
-FZF_ALT_C_COMMAND="fd -H --exclude=node_modules --exclude=Trash --type=d "
-zinit has"fzf" light-mode for \
-  https://raw.githubusercontent.com/junegunn/fzf/master/shell/key-bindings.zsh
+# FZF_CTRL_T_COMMAND="fd -H --exclude=node_modules --exclude=Trash"
+# FZF_ALT_C_COMMAND="fd -H --exclude=node_modules --exclude=Trash --type=d "
+# zinit has"fzf" light-mode for \
+#   https://raw.githubusercontent.com/junegunn/fzf/master/shell/key-bindings.zsh
 
 # MINICONDA3 -------------------------------------------------------------------
 [ -f /opt/miniconda3/etc/profile.d/conda.sh ] \
