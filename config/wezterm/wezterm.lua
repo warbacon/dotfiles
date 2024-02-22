8c560c6471d1d5db13f32f05c8191538384f8f1a
Warbacon <45147327+warbacon@users.noreply.github.com>
2024-02-22 19:58:21 +0100

    Update wezterm config

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,47 +1,32 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This table will hold the configuration.
-local config = {}
+-- This will hold the configuration.
+local config = wezterm.config_builder()
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-if wezterm.config_builder then
-	config = wezterm.config_builder()
-end
-
--- Rendering
--- config.front_end = "WebGpu"
-
--- Shell
+-- GENERAL
+config.check_for_updates = false
 config.default_prog = { "fish" }
--- config.default_prog = { "pwsh", "-NoLogo" }
-
--- Font
-config.font = wezterm.font_with_fallback({ "Fira Code", "Symbols Nerd Font" })
-config.font_size = 16
+config.front_end = "WebGpu"
+config.term = "wezterm"
 config.unicode_version = 14
 config.warn_about_missing_glyphs = false
 
 -- UI
 config.color_scheme = "Catppuccin Mocha"
-config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
-}
-config.bold_brightens_ansi_colors = false
-config.animation_fps = 1
-config.default_cursor_style = "BlinkingBar"
-config.cursor_blink_ease_in = "Constant"
-config.cursor_blink_ease_out = "Constant"
-config.cursor_thickness = 3
+config.font = wezterm.font_with_fallback({
+	"Fira Code",
+	"Symbols Nerd Font",
+	"Noto Color Emoji",
+})
+config.font_size = 14.5
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_max_width = 32
+config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 
--- Keymaps
+-- KEYBINDINGS
 config.keys = {
 	{
 		key = "w",
@@ -49,6 +34,15 @@ config.keys = {
 		action = wezterm.action.CloseCurrentPane({ confirm = true }),
 	},
 }
+
+-- ALT + number to activate that tab
+for i = 1, 9 do
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "ALT",
+		action = wezterm.action.ActivateTab(i - 1),
+	})
+end
 
 -- and finally, return the configuration to wezterm
 return config
