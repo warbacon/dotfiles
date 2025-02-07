c05fc5a95ad0d19552f708c8f35c95befbb15f8f
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-02-07 20:10:36 +0100

    mystart: fix gtk font setting

--- mystart/mystart/modules/gtk.py
+++ mystart/mystart/modules/gtk.py
@@ -7,12 +7,11 @@ from mystart.util import (
 )
 
 pkgs = ["adw-gtk-theme", "inter-font"]
-
 install_packages(pkgs)
 
 gsettings_set("org.gnome.desktop.interface", "gtk-theme", "adw-gtk3-dark")
 gsettings_set("org.gnome.desktop.interface", "color-scheme", "prefer-dark")
-gsettings_set("org.gnome.desktop.interface", "font-name", "Inter Variable 10")
+gsettings_set("org.gnome.desktop.interface", "font-name", "'Inter Variable 10'")
 
 gtk3_dir = f"{home_dir}/.config/gtk-3.0"
 gtk3_config = f"{gtk3_dir}/settings.ini"
