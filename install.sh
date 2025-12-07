696b7a84ae62dccc33906a3d5731e10476ec5a87
Joaquín Guerra <joaquinguerratocino@gmail.com>
2025-12-07 19:52:28 +0100

    install: run stow first

--- install.sh
+++ install.sh
@@ -8,6 +8,10 @@ ensure_dir "$HOME/.config"
 ensure_dir "$HOME/.local/bin"
 export PATH="$HOME/.local/bin:$PATH"
 
+info "Doing stow things..."
+install stow
+stow warbacon -v
+
 run_logged ./modules/system.sh
 run_logged ./modules/fonts.sh
 run_logged ./modules/gpu.sh
@@ -20,7 +24,3 @@ run_logged ./modules/laptop.sh
 
 info "Building mandb..."
 sudo mandb -q
-
-info "Doing stow things..."
-install stow
-stow warbacon -v
